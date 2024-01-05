5a61ca2d5d610a423f0c48abd0fc3f678a2d2185
Ilya M <wuvuxd@proton.me>
2024-01-05 21:27:17 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import '../app.css';
+</script>
+
+<main>
+  <h1>Aboba</h1>
+  <div>
+    <slot />
+  </div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  let name = 'Ilya';
+  let surname = 'Mazunin';
+
+  $: fullName = `${name} ${surname}`;
+</script>
+
+<h1>Hello, {fullName}</h1>
+<button on:click={() => (surname = 'Kyubey')}> Change name </button>
+
+<h1 class="heading">Hello world!</h1>
+
+<style lang="postcss">
+  .heading {
+    @apply text-3xl font-bold underline text-red-600;
+  }
+
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
