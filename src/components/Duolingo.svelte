45366c52a85a8deae44ed01be95596ff4936241a
Ilya M <wuvuxd@proton.me>
2024-01-10 01:25:00 +0700

    move duolingo to separate component

---
+++ src/components/Duolingo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const MSECONDS_IN_ONE_DAY = 86400000;
+  const startDate = '2023-06-24T00:00:00.000Z';
+  let currentDate = +new Date();
+  let days = Math.round((currentDate - Date.parse(startDate)) / MSECONDS_IN_ONE_DAY);
+</script>
+
+<span class="combo">{days}</span>
+
+<style lang="postcss">
+  .combo {
+    @apply font-rocket text-reseda_green;
+  }
+</style>
