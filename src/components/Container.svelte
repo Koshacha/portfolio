32bade2b9f73f193a229658b068f6599727ab8bf
Ilya M. <wuvuxd@proton.me>
2024-01-18 16:56:53 +0700

    mv container styling to component

---
+++ src/components/Container.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import cn from 'classnames';
+  export let fluid = false;
+</script>
+
+<div
+  class={cn('wrapper', {
+    'wrapper--fluid': fluid
+  })}
+>
+  <slot />
+</div>
+
+<style lang="postcss">
+  .wrapper {
+    @apply container mx-auto;
+  }
+
+  .wrapper--fluid {
+    @apply max-w-full;
+  }
+</style>
