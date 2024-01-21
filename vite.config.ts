0a0aa6c9f9253e65edbda2ab5e824d8055f71dd1
Ilya M. <wuvuxd@proton.me>
2024-01-21 23:37:33 +0700

    include image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), ViteImageOptimizer()]
 });
