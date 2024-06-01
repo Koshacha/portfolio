ccb4af2169679518e090ee38830679a8a14b0e16
Ilya M. <wuvuxd@proton.me>
2024-06-01 16:48:01 +0700

    add pinia

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -30,7 +30,7 @@
   }
 
   .icon :global(.icon__img) {
-    @apply h-9 w-9 rounded-sm object-contain grayscale-[0.6];
+    @apply h-9 w-9 rounded-sm object-contain grayscale-[0.5];
   }
 
   .icon :global(.icon__img__hovered) {
